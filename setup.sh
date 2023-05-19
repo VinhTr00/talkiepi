6b0a847e650b7487c9e3da07ec9a8b214cc17ad7
Vinh Trần Hiển <tomoki159369@gmail.com>
2023-05-19 22:01:08 +0700

    add new service

--- setup.sh
+++ setup.sh
@@ -4,11 +4,19 @@ echo "Create mumble.service"
 
 sudo cp ~/Documents/code-talkiepi/gocode/src/github.com/VinhTr00/talkiepi/conf/systemd/mumble.service /etc/systemd/system/mumble.service
 
+sudo cp ~/Documents/code-talkiepi/gocode/src/github.com/VinhTr00/talkiepi/conf/systemd/restart_mumble.service /etc/systemd/system/restart_mumble.service
+
+sudo cp ~/Documents/code-talkiepi/gocode/src/github.com/VinhTr00/talkiepi/conf/systemd/restart-mumble.sh /usr/bin/restart_mumble.sh
+
+sudo chmod +x  /usr/bin/restart_mumble.sh
+
 sudo systemctl daemon-reload
 
 sudo systemctl enable mumble.service
+sudo systemctl enable restart_mumble.service
 
 sudo systemctl start mumble.service
+sudo systemctl start restart_mumble.service
 
 echo "###############################"
 echo "### Setup Service Completed ###"
